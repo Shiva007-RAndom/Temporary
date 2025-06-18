aac1ef7f3d810329c25591786f5282fac6200cfe
Shiva007-RAndom <sivasubramanians2006@gmail.com>
2025-06-19 01:06:08 +0530

    Update tess.py

--- Langchain_app/tess.py
+++ Langchain_app/tess.py
@@ -1,12 +1,14 @@
 import pytesseract
 from pathlib import Path
-from PIL import Image
-import pdf2image
+from PIL import Image,ImageFilter
 
 def ocr():
     text = []
-    imgs = Path('D:/ML/Task-2/imgs').glob('*')
-    for img in imgs:
-        image = Image.open(img)
-        text.append(pytesseract.image_to_string(image))
+    imgs = Path('D:/ML/Langchain_app/imgs').glob('*')
+    for i in imgs:
+        img = Image.open(i)
+        img = img.convert('L')
+        img = img.filter(ImageFilter.UnsharpMask(1.5,200,2))
+        text.append(pytesseract.image_to_string(img))
+        #img.show()
     return text
